78e644c394512030d4ac6f8b7593198cce9ea026
Rui Zhang <ryanzh@umich.edu>
2017-09-26 19:40:28 -0400

    first commit

--- code/run_interleaveGRUcollab_match_CAND2_Nc15.sh
+++ code/run_interleaveGRUcollab_match_CAND2_Nc15.sh
@@ -3,9 +3,9 @@
 THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32 \
 python -m adr_res_selection.main.main \
        -mode train \
-       --train_data ../data/input/train-data.cand-10.gz \
-       --dev_data ../data/input/dev-data.cand-10.gz \
-       --test_data ../data/input/test-data.cand-10.gz \
+       --train_data ../data/input/train-data.cand-2.gz \
+       --dev_data ../data/input/dev-data.cand-2.gz \
+       --test_data ../data/input/test-data.cand-2.gz \
        --model interleavecollab \
        --unit gru \
        --batch 128 \
@@ -14,5 +14,5 @@ python -m adr_res_selection.main.main \
        --n_prev_sents 15 \
        --crosstest 4 \
        --save 1 \
-       --output_fn model_collab_CAND10_Nc15.pkl.gz \
-       | tee interleaveGRUcollab_match_CAND10_Nc15.txt
+       --output_fn model_collab_CAND2_Nc15.pkl.gz \
+       | tee interleaveGRUcollab_match_CAND2_Nc15.txt
